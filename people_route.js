5029f06e6c1b261ac6c0f3692781fdf9dcff8b94
miwzerking <0865697@students.dadeschools.net>
2020-11-18 23:08:30 -0500

    finished router for people table

--- people_route.js
+++ people_route.js
@@ -1,8 +1,11 @@
 const people_router = require('express').Router();
 
 const db = require('./queries');
-
+people_router.param(':id', db.idParams);
 people_router.get('/', db.getAllPeople);
-
+people_router.get('/:id', db.getSinglePerson);
+people_router.put('/:id', db.updatePersonInfo);
+people_router.post('/', db.createNewPerson);
+people_router.delete('/:id', db.deletePerson);
 
 module.exports = people_router;
--- queries.js
+++ queries.js
@@ -1,7 +1,12 @@
 const pool = require('./configure_sql');
 
+const idParams = (req, res, next, ID) => {
+    req.id = req.params.id;
+    next();
+}
+
 const getAllPeople = (req, res) => {
-    pool.query('SELECT * FROM people', (err, results) => {
+    pool.query('SELECT * FROM people ORDER BY id', (err, results) => {
         if(err) {
             throw err;
         }
@@ -9,6 +14,48 @@ const getAllPeople = (req, res) => {
     })
 }
 
+const getSinglePerson = (req, res) => {
+    pool.query('SELECT * FROM people WHERE id = $1', [req.id], (err, results) => {
+        if (err) {
+            throw err;
+        }
+        res.send(results.rows);
+    })
+}
+
+const updatePersonInfo = (req, res) => {
+    const {name, email} = req.body;
+    pool.query('UPDATE people SET name = $1, email = $2 WHERE id = $3', [name, email, req.id], (err, results) => {
+        if(err) {
+            throw err;
+        }
+        res.send(`you have updated person with id: ${req.id}, you may get router to see the result`);
+    })
+}
+
+const createNewPerson = (req, res) => {
+    const {name, email} = req.body;
+    pool.query('INSERT INTO people (name, email)' 
+    + 'VALUES ($1, $2)',
+    [name, email], (err, results) => {
+        if(err) {
+            throw err;
+        }
+        res.send('you have successful created new person record');
+    })
+}
+
+const deletePerson = (req, res) => {
+    pool.query('DELETE people WHERE id = $1', [req.id], (err, results) => {
+        res.send(`you have deleted person with id:${req.id}`);
+    })
+}
+
 module.exports = {
-    getAllPeople
+    idParams,
+    getAllPeople,
+    getSinglePerson,
+    updatePersonInfo,
+    createNewPerson,
+    deletePerson
 }
